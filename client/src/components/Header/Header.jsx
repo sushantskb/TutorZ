26778b0642472954c6be7ade2215a1c14c7e87d4
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-03 16:38:00 +0530

    added media query to navbar and also the hamburger menu

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,7 +4,12 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [menu, setMenu] = useState(false);
 
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  }
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 0);
@@ -20,15 +25,14 @@ const Header = () => {
     <nav className={isScrolled ? "window-scroll" : ""}>
       <div className="container nav_container">
         <a href="index.html"><h4>TutorZ</h4></a>
-        <ul className="nav_menu">
+        <ul className={menu ? "nav_menu open" : "nav_menu"}>
           <li><Link to="/home">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/courses">Courses</Link></li>
           <li><Link to="/tutors">Tutors</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
-        <button id="open-menu-btn"><i className="uil uil-bars"></i></button>
-        <button id="close-menu-btn"><i className="uil uil-multiply"></i></button>
+        <button id="open-menu-btn" onClick={toggleMenu}>{!menu ? <i className="uil uil-bars"></i> : <i className="uil uil-multiply"></i>}</button>
       </div>
     </nav>
   )
