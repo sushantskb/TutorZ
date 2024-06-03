1ae26fd3f102d7ad5bb2ad003936634f65216e2c
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-03 10:56:47 +0530

    added the student profile page

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home/Home"
 import Tutors from "../pages/Tutors/Tutors"
 import NotFound_404 from "../pages/NotFound/NotFound_404"
+import StudentProfile from "../pages/Profiles/StudentProfile/StudentProfile"
 
 const Routers = () => {
   return (
@@ -9,6 +10,7 @@ const Routers = () => {
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/tutors" element={<Tutors />} />
+      <Route path="/student" element={<StudentProfile />} />
       <Route path="/*" element={<NotFound_404 />} />
     </Routes>
   )
