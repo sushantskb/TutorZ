ef025dca1ffbf044a6f1c760a2b799be956b7d06
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-03 11:50:37 +0530

    added the tutor profile page

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home/Home"
 import Tutors from "../pages/Tutors/Tutors"
 import NotFound_404 from "../pages/NotFound/NotFound_404"
 import StudentProfile from "../pages/Profiles/StudentProfile/StudentProfile"
+import TutorProfile from "../pages/Profiles/TutorProfile/TutorProfile"
 
 const Routers = () => {
   return (
@@ -11,6 +12,7 @@ const Routers = () => {
       <Route path="/home" element={<Home />} />
       <Route path="/tutors" element={<Tutors />} />
       <Route path="/student" element={<StudentProfile />} />
+      <Route path="/tutor" element={<TutorProfile />} />
       <Route path="/*" element={<NotFound_404 />} />
     </Routes>
   )
