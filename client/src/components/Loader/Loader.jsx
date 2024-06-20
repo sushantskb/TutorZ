e396129d7ac07b427c4bb095ebf99670ad1e052b
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-21 00:15:29 +0530

    added the loader component

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import "./loader.css"; // CSS file for styling the loader
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loader;
