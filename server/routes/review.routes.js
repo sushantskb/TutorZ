7ed4a9603afb3a5789787cf1f159a79f5a531598
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-06-24 14:41:22 +0530

    updated the controller and routes for review

--- server/routes/review.routes.js
+++ server/routes/review.routes.js
@@ -17,19 +17,4 @@ router.post("/create-review/:id", authenticate, createReview); // this is teache
 // Fetch all reviews of a teacher
 router.get("/all-reviews/:id", authenticate, getAllReviews);
 
-// Fetch a particular review
-router.get("/review/:id", authenticate, getReviewById); // this is review id
-
-// Update a Review
-router.put("/update-review/:id", authenticate, updateReview); // this is review id
-
-// Delete a review
-router.delete("/delete-review:id", authenticate, deleteReview);
-
-// Like a review
-router.post("/like/:id", authenticate, likeReview); // should follow this convention
-
-// Reply to review
-router.post("/reply/:id", authenticate, replyToReview); // should follow this convention
-
 module.exports = router;
