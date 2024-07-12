50818b4325c7c0f7d12061cc48c64fd2d3bf42e6
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-12 16:38:28 +0530

    added the contact backend and integrated it

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const reviewRoutes = require("./routes/review.routes");
 const slotRoutes = require("./routes/slot.routes");
 const bookingRoutes = require("./routes/booking.routes");
 const webhookRoutes = require("./routes/webhooks.routes");
+const { sendMessage } = require("./controllers/contact.controllers");
 const app = express();
 const PORT = process.env.PORT || 6969;
 
@@ -33,7 +34,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/slots", slotRoutes);
 app.use("/api/bookings", bookingRoutes);
-
+app.post("/api/contact", sendMessage);
 connDB();
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`.blue);
